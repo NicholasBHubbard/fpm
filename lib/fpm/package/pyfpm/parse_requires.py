29389369139d8f5fbc00c44abf1a398b0bf80d3b
Nicholas B. Hubbard <nicholashubbard@posteo.net>
2025-11-19 18:00:50 -0500

    correct parse_requires

--- lib/fpm/package/pyfpm/parse_requires.py
+++ lib/fpm/package/pyfpm/parse_requires.py
@@ -2,6 +2,7 @@
 
 #import pkg_resources
 import packaging.requirements
+import packaging.markers
 import json
 import sys
 
@@ -11,18 +12,14 @@ import sys
 # Process environment markers, if any, and produce a list of requirements for the current environment.
 def evaluate_requirements(fd):
     all_requirements = [packaging.requirements.Requirement(line) for line in sys.stdin]
-    
+    env = packaging.markers.default_environment()
     for req in all_requirements:
         # XXX: Note: marker.evaluate() can be given a dict() containing environment values to overwrite
-        try:
-            if req.marker is None or req.marker.evaluate():
-                if len(req.specifier) > 0:
-                    for spec in req.specifier:
-                        yield "%s%s" % (req.name, spec)
-                else:
-                    yield str(req.name)
-        except packaging.markers.UndefinedEnvironmentName:
-            # Include requirement if we can't evaluate the marker
-            yield str(req)
+        if req.marker is None or req.marker.evaluate(environment=env):
+            if len(req.specifier) > 0:
+                for spec in req.specifier:
+                    yield "%s%s" % (req.name, spec)
+            else:
+                yield str(req.name)
 
 print(json.dumps(list(evaluate_requirements(sys.stdin))))
